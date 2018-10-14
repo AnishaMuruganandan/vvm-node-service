0b4a39cdf9385e98ce01f53751cf96ef8ee211e6
AnishaMuruganandan <anisha95.m@gmail.com>
2018-10-14 19:05:19 +0530

    VVM : adding new text local

--- util/otp.js
+++ util/otp.js
@@ -7,7 +7,7 @@ var validOptions = {
   apikey: 'KwnomGVgP2s-ZEItpEPgykuxybij9puVCXC730R6sc'
 };
 // var validOptions = {  };
-var tl = require('TextLocal')(validOptions);
+// var tl = require('TextLocal')(validOptions);
 
 var OTPUtil = (function() {
   var otp;
@@ -20,11 +20,11 @@ var OTPUtil = (function() {
 
   let SendingOTP = function(OTP, phoneNo) {
     var result;
-    tl.sendSMS(phoneNo,  OTP + " is the OTP for Crop Per Drop App.", 'TeamBF', function(err, data) {
-      console.log("err" + err + "data" + data);
-       result = data;
-    });
-    return result;
+    // tl.sendSMS(phoneNo,  OTP + " is the OTP for Crop Per Drop App.", 'TeamBF', function(err, data) {
+    //   console.log("err" + err + "data" + data);
+    //    result = data;
+    // });
+    // return result;
     const TEXT_LOCAL_HOST = 'https://api.textlocal.in/send/';
     var MSG_TEMPLATE = OTP + " is the OTP for Crop Per Drop App.";
     var TEXT_LOCAL_SENDER = 'TeamBF';
