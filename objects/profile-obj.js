2279748ee945a37198c3baf1c63d80aa58730f3f
Ashwin Vishwakarma <ashwin.shekher@gmail.com>
2018-10-23 18:00:34 +0530

    VVM : Code Correction

--- objects/profile-obj.js
+++ objects/profile-obj.js
@@ -1,33 +1,34 @@
 var profileObj = {
 
-    saveBasicProfileDetailsObj: function(name, dateOfBirth, height, cityId, educationId, professionId, religionId, casteId, profilePicture) {
-      this.name = name || '0';
-      this.dateOfBirth = dateOfBirth || '0';
-      this.height = height || '0';
-      this.cityId = cityId || '0';
-      this.educationId = educationId || '0';
-      this.professionId = professionId || '0';
-      this.religionId = religionId || '0';
-      this.casteId = casteId || '0';
-      this.profilePicture = profilePicture || '0';
-    }
+  saveBasicProfileDetailsObj: function(name, dateOfBirth, height, cityId, educationId, professionId, religionId, casteId, profilePicture) {
+    this.name = name || '0';
+    this.dateOfBirth = dateOfBirth || '0';
+    this.height = height || '0';
+    this.cityId = cityId || '0';
+    this.educationId = educationId || '0';
+    this.professionId = professionId || '0';
+    this.religionId = religionId || '0';
+    this.casteId = casteId || '0';
+    this.profilePicture = profilePicture || '0';
+  },
 
-    saveProfileDetailsObj: function(name, dateOfBirth, age, religionId, gender, educationId, professionId, salaryId, height, complexion, cityId, address, physicalDisablity, profilePicture) {
-      this.name = name || '0';
-      this.dateOfBirth = dateOfBirth || '0';
-      this.age = age || '0';
-      this.religionId = religionId || '0';
-      this.gender = gender || '0';
-      this.educationId = educationId || '0';
-      this.professionId = professionId || '0';
-      this.salaryId = salaryId || '0';
-      this.height = height || '0';
-      this.complexion = complexion || '0';
-      this.cityId = cityId || '0';
-      this.address = address || '0';
-      this.physicalDisablity = physicalDisablity || '0';
-      this.profilePicture = profilePicture || '0';
+  saveProfileDetailsObj: function(name, dateOfBirth, age, religionId, gender, educationId, professionId, salaryId, height, complexion, cityId, address, physicalDisablity, profilePicture) {
+    this.name = name || '0';
+    this.dateOfBirth = dateOfBirth || '0';
+    this.age = age || '0';
+    this.religionId = religionId || '0';
+    this.gender = gender || '0';
+    this.educationId = educationId || '0';
+    this.professionId = professionId || '0';
+    this.salaryId = salaryId || '0';
+    this.height = height || '0';
+    this.complexion = complexion || '0';
+    this.cityId = cityId || '0';
+    this.address = address || '0';
+    this.physicalDisablity = physicalDisablity || '0';
+    this.profilePicture = profilePicture || '0';
 
-    }
+  },
+}
 
-    module.exports = profileObj;
+module.exports = profileObj;
--- service/profile-service.js
+++ service/profile-service.js
@@ -1,55 +1,55 @@
 var express = require('express');
 var profileQueries = require('../models/profile-db-queries');
-var profileQueriesConst = require('../query-constants/profile-query-constants');
+var profileQueriesConst = require('../query-constants/profile-query-constant');
 var profileService = (function() {
 
-  saveBasicProfileDetails: async function(saveBasicProfileDetailsObj) {
-      console.log("profileService");
-      var msg = "";
-      try {
-        var output = await profileQueries.saveBasicDetailsQuery(profileQueriesConst.saveBasicDetailsQuery, saveBasicProfileDetailsObj);
-        console.log(JSON.stringify(output) + "output");
-        msg = "profile" + output.profileId;
-      } catch (err) {
-        msg = err;
-        console.log('Opps, an error occurred', err);
-      }
-      return msg;
-    };
+  saveBasicProfileDetails = async function(saveBasicProfileDetailsObj) {
+    console.log("profileService");
+    var msg = "";
+    try {
+      var output = await profileQueries.saveBasicDetailsQuery(profileQueriesConst.saveBasicDetailsQuery, saveBasicProfileDetailsObj);
+      console.log(JSON.stringify(output) + "output");
+      msg = "profile" + output.profileId;
+    } catch (err) {
+      msg = err;
+      console.log('Opps, an error occurred', err);
+    }
+    return msg;
+  };
 
-  saveProfileDetails: async function(saveProfileDetailsObj) {
-      console.log("profileService");
-      var msg = "";
-      try {
-        var output = await profileQueries.saveAll(profileQueriesConst.saveAllQuery, saveProfileDetailsObj);
-        console.log(JSON.stringify(output) + "output");
-        msg = "profile" + output.profileId;
-      } catch (err) {
-        msg = err;
-        console.log('Opps, an error occurred', err);
-      }
-      return msg;
-    };
+  saveProfileDetails = async function(saveProfileDetailsObj) {
+    console.log("profileService");
+    var msg = "";
+    try {
+      var output = await profileQueries.saveAll(profileQueriesConst.saveAllQuery, saveProfileDetailsObj);
+      console.log(JSON.stringify(output) + "output");
+      msg = "profile" + output.profileId;
+    } catch (err) {
+      msg = err;
+      console.log('Opps, an error occurred', err);
+    }
+    return msg;
+  };
 
-    getProfileDetailsByProfileId: async function() {
-        console.log("profileService");
-        var msg = "";
-        try {
-          var output = await profileQueries.getProfileDetails(profileQueriesConst.getProfileDetailsQuery);
-          console.log(JSON.stringify(output) + "output");
-          msg = "profile" + output.profileId;
-        } catch (err) {
-          msg = err;
-          console.log('Opps, an error occurred', err);
-        }
-        return msg;
-      }
+  getProfileDetailsByProfileId = async function() {
+    console.log("profileService");
+    var msg = "";
+    try {
+      var output = await profileQueries.getProfileDetails(profileQueriesConst.getProfileDetailsQuery);
+      console.log(JSON.stringify(output) + "output");
+      msg = "profile" + output.profileId;
+    } catch (err) {
+      msg = err;
+      console.log('Opps, an error occurred', err);
+    }
+    return msg;
+  }
 
-      return {
-        saveProfileDetails: saveProfileDetails,
-        getProfileDetailsByProfileId: getProfileDetailsByProfileId
-      }
+  return {
+    saveProfileDetails: saveProfileDetails,
+    getProfileDetailsByProfileId: getProfileDetailsByProfileId
+  }
 
-    })();
+})();
 
 module.exports = profileService;
