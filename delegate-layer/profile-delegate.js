a908e77730069b6001e2bb6d82381c37eef7bc54
Ashwin Vishwakarma <ashwin.shekher@gmail.com>
2018-10-24 10:47:48 +0530

    VVM : Code changes

--- delegate-layer/profile-delegate.js
+++ delegate-layer/profile-delegate.js
@@ -4,7 +4,7 @@ var profileObject = require('../objects/profile-obj');
 
 var profileDelegate = (function() {
 
-  saveBasicProfileDetails: async function(profileDetails) {
+  saveBasicProfileDetails = async function(profileDetails) {
     console.log("dlegate");
     var saveBasicProfileDetailsObj = new profileObject.saveBasicProfileDetailsObj(profileDetails.name,
       profileDetails.dob,
@@ -27,7 +27,7 @@ var profileDelegate = (function() {
   };
 
 
-  saveMoreProfileDetails: async function(profileDetails) {
+  saveMoreProfileDetails = async function(profileDetails) {
     console.log("dlegate");
     var saveProfileDetailsObj = new profileObject.saveProfileDetailsObj(profileDetails.name,
       profileDetails.dob,
@@ -54,7 +54,7 @@ var profileDelegate = (function() {
     }
   };
 
-  getProfileDetailsByProfileId: async function(profileDetails) {
+  getProfileDetailsByProfileId = async function(profileDetails) {
     console.log("dlegate");
     try {
       console.log("del try");
