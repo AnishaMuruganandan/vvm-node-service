62db09d62d0dabd81a8e96dadd947df5983b5c54
Ashwin Vishwakarma <ashwin.shekher@gmail.com>
2018-10-26 09:06:56 +0530

    VVM: Login Issue Fixed

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,2 +1,3 @@
 //URL_Domain = "https://vivahahvizhamatrimony.herokuapp.com/";
- URL_Domain = "http://localhost:3000/";
+//URL_Domain = "http://localhost:3000/";
+URL_Domain = window.location.origin + "/";
