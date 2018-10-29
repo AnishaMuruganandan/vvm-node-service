e6dd927d708e463ff5b3d4ccef9d636501bceb0b
Ashwin Vishwakarma <ashwin.shekher@gmail.com>
2018-10-29 07:58:03 +0530

    VVM : Fetch details

---
+++ public/js/profile-utils.js
@@ -0,0 +1,39 @@
+var profileUtils = (function() {
+
+  $upload_widget_opener = $('#upload_widget_opener');
+  $fullName = $('#fullName');
+  $livingPlace = $('#livingPlace');
+  $profileViewId = $('#profileViewId');
+  $age = $('#age');
+  $contactNo = $('#contactNo');
+  $profession = $('#profession');
+  $education = $('#education');
+  $religion = $('#religion');
+  $caste = $('#caste');
+
+  $name = $('#name');
+  $gender = $('#gender');
+  $salary = $('#salary');
+  $complexion = $('#complexion');
+  $address = $('#address');
+  $physicallyDisabled = $('#physicallyDisabled');
+  $languageKnown = $('#languageKnown');
+  $imageURL = $('#image-url');
+  $horoscope = $('#horoscope');
+  $cityLiving = $('#cityLiving');
+  $profession = $('#profession');
+  $education = $('#education');
+  $fatherName = $('#fName');
+  $motherName = $('#mName');
+  $siblingCount = $('#sibCount');
+  $motherTongue = $('#motherTongue');
+  $nativePalce = $('#nativePlace');
+  $jathagamSoftCopy = $('#horoscope');
+  $tob = $('#tob');
+  $pob = $('#pob');
+  $zodiac = $('#rasi');
+  $star = $('#nakshatra');
+  $lagna = $('#lagna');
+
+
+})();
